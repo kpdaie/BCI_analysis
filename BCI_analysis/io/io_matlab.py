1a0f2c7095bad582f3fc7536e6719f79f875a2fc
rozmar <qtyush@gmail.com>
2022-02-09 15:38:09 -0800

    more explanation in matlab load function, transposed variables flattened out

--- BCI_analysis/io/io_matlab.py
+++ BCI_analysis/io/io_matlab.py
@@ -14,19 +14,36 @@ def read_multisession_mat(full_file_path):
     Returns
     -------
     data_dict : dict
-        file_path - Path to loaded file
-        file_name - Name of loaded file
-        n_days - Number of sessions in this dataset
-        cni - Conditioned Neuron index
-        dFF - DeltaF/F      [time points x neurons]
-        dist - Distance from Conditioned Neuron in pixels   [neurons x 1]
-        meanImg - Mean Image of the roi
-        raw - Flourescence Intensity    [time points x neurons]
-        F_trialwise - DeltaF/F reshaped     [time points x neurons x trials]
-        tsta - Time steps corresponding to F
-        total_steps - Total time steps
-        n_trials - Number of trials
-        epoch - 3 keys corresponding to spontaneuous Pre Training, closed loop, spontaneous Post Training [trials x 1]
+        file_path - str,
+            Path to loaded file
+        file_name - str
+            Name of loaded file
+        n_days - int
+            Number of sessions in this dataset
+        cni - int, [sessions]
+            Conditioned Neuron indices
+        dFF - float [sessions x time points x neurons]
+            DeltaF/F      
+        dist -float [sessions x neurons x 1]
+            Distance from Conditioned Neuron in pixels   
+        meanImg - float [sessions x Y_size x X_size]
+            Mean Image of the field of view
+        raw - float [sessions x time points x neurons]
+            Flourescence Intensity
+        F_trialwise - float [sessions x time points x neurons x trials]
+            DeltaF/F reshaped     
+        tsta - float [sessions x time points]
+            Time steps corresponding to F_trialwise
+        total_steps - int [sessions]
+            Total time steps
+        n_trials - int [sessions]
+            Number of trials
+        epoch - int, [sessions x trials]
+            3 keys corresponding to spontaneuous Pre Training, closed loop, spontaneous Post Training 
+        lkeys - int [sessions]
+            which key correspond to actual training trials from epoch
+        epoch_closedloop bool [sessions]
+            ???
     """
     file_path,file_name = os.path.split(full_file_path)
     data=io.loadmat(full_file_path)
@@ -34,14 +51,14 @@ def read_multisession_mat(full_file_path):
     n_days = data['data'].shape[1]    
     cni = [data['data'][:, i]['cn'][0][0][0] - 1 for i in range(n_days)] # -1 to account for matlab indexing
     dFF = [data['data'][:, i]['df'][0] for i in range(n_days)]
-    dist = [data['data'][:, i]['dist'][0] for i in range(n_days)]
+    dist = [data['data'][:, i]['dist'][0].flatten() for i in range(n_days)]
     meanImg = [data['data'][:, i]['IM'][0] for i in range(n_days)]
     raw = [data['data'][:, i]['raw'][0] for i in range(n_days)]
     F_trialwise = [data['data'][:, i]['F'][0] for i in range(n_days)]
-    tsta = [data['data'][:, i]['tsta'][0] for i in range(n_days)]
+    tsta = [data['data'][:, i]['tsta'][0].flatten() for i in range(n_days)]
     total_steps = [dFF[i].shape[0] for i in range(n_days)]
     n_trials = [F_trialwise[i].shape[-1] for i in range(n_days)]
-    epoch = [data['data'][:, i]['epoch'][0] for i in range(n_days)]
+    epoch = [data['data'][:, i]['epoch'][0].flatten() for i in range(n_days)]
     lkeys = [[1,3], 1, 4, 2]  #Since i did not know the exact epoch numbers for the BCI_13Sep session, 
     # I hardcoded them here. This should change in the future
 
