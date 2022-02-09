cc75a29b5de0af04bacdeecbb4eb7d395c0cd920
Mohit Kulkarni <mohitmk@iitk.ac.in>
2022-02-09 09:01:29 +0530

    Updated docstrings for read_multisession_mat

--- BCI_analysis/io/io_matlab.py
+++ BCI_analysis/io/io_matlab.py
@@ -14,22 +14,19 @@ def read_multisession_mat(full_file_path):
     Returns
     -------
     data_dict : dict
-        file_path - path to loaded file
-        file_name - name of loaded file
-        n_days - number of sessions in this dataset
-        cni - 
-        dFF - 
-        dist - 
-        meanImg - 
-        raw - 
-        F_trialwise - 
-        tsta - 
-        total_steps - 
-        n_trials - 
-        epoch - 
-        lkeys - 
-        epoch_closedloop - 
-
+        file_path - Path to loaded file
+        file_name - Name of loaded file
+        n_days - Number of sessions in this dataset
+        cni - Conditioned Neuron index
+        dFF - DeltaF/F      [time points x neurons]
+        dist - Distance from Conditioned Neuron in pixels   [neurons x 1]
+        meanImg - Mean Image of the roi
+        raw - Flourescence Intensity    [time points x neurons]
+        F_trialwise - DeltaF/F reshaped     [time points x neurons x trials]
+        tsta - Time steps corresponding to F
+        total_steps - Total time steps
+        n_trials - Number of trials
+        epoch - 3 keys corresponding to spontaneuous Pre Training, closed loop, spontaneous Post Training [trials x 1]
     """
     file_path,file_name = os.path.split(full_file_path)
     data=io.loadmat(full_file_path)
@@ -45,7 +42,9 @@ def read_multisession_mat(full_file_path):
     total_steps = [dFF[i].shape[0] for i in range(n_days)]
     n_trials = [F_trialwise[i].shape[-1] for i in range(n_days)]
     epoch = [data['data'][:, i]['epoch'][0] for i in range(n_days)]
-    lkeys = [[1,3], 1, 4, 2]
+    lkeys = [[1,3], 1, 4, 2]  #Since i did not know the exact epoch numbers for the BCI_13Sep session, 
+    # I hardcoded them here. This should change in the future
+
     epoch_closedloop = [np.isin(epoch[i],lkeys[i])[0] for i in range(n_days)]
     
     
