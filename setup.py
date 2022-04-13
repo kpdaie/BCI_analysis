02a91c5622f4c5c6f51f2a5caaf573512305ea2c
rozmar <qtyush@gmail.com>
2022-04-13 09:45:40 -0700

    added necessary packages

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='BCI_analysis',
     packages=find_packages(),
-)
+    install_requires=['scipy',
+                      'pandas',
+                      'mat73',
+                      'scanimage_tiff_reader',
+                      'pybpod']
+    )
