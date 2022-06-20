f84a6a176ddde30882dbf61c5fbabad520c07aac
rozmar <qtyush@gmail.com>
2022-06-20 00:07:22 -0700

    io_python was missing from _init_

--- BCI_analysis/__init__.py
+++ BCI_analysis/__init__.py
@@ -1,3 +1,3 @@
-from .io import io_matlab, io_pybpod, io_utils, io_scanimage, io_suite2p
+from .io import io_matlab, io_python, io_pybpod, io_utils, io_scanimage, io_suite2p
 from .pipeline import pipeline_bpod, pipeline_imaging
 from .plot import plot_imaging
